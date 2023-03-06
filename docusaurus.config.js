365ff7771ce2cf1a211e4b036bdb04cb3c9256fb
Brian Johnson <brianjo@fb.com>
2023-03-06 17:30:55 -0500

    updates

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Jupisaurus',
-  tagline: 'Publishing Jupyter notebooks in Docuasurus',
+  title: 'PyTorch Basics',
+  tagline: 'An introduction to PyTorch',
   favicon: 'img/favicon.png',
 
   // Set the production url of your site here
